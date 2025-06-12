c86a257efb1a980408ca344de93967b998cc2ecc
khushi-9504 <khushaligajera028wdt@gmail.com>
2025-06-12 22:24:07 +0530

    loops and dom

---
+++ JS Day 5/loops.js
@@ -0,0 +1,63 @@
+// Truthy values : true, 1, "0", 'false', " ", [], {}, function(){}
+
+// falsy values : false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Nullish Coalescing Operator ( ?? ) : null and undefined
+
+// val1 = null ?? 10	//10
+// val1 = undefined ?? 15		//15
+
+
+// For, While and do-while loop, break and continue
+
+// High Order Array Loops : 
+
+// for...of - arrays (iteratable objects)
+// for...in - objects
+
+// higher order function : 
+
+// forEach -  
+
+
+// map() : 
+// filter() : 
+// reduce() : 
+
+
+// DOM Manipulation : 
+
+// what is dom?
+// How to select an element using dom?
+// getElementById, getElementByClassName, getElementByTagName, querySelector, querySelectorAll
+// textContent, innerHTML, innerText
+// NodeList, HTMLCollection
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`Outer Loop ${i}`);
+  
+//   for (let j = 1; j <= 5; j++) {
+//     console.log(`Outer Loop ${i} Inner Loop ${j}`);
+//   }
+// }
+
+// let i=1;
+// while(i<10){
+//   console.log(i);
+//   i++;
+// }
+
+// let myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// let nuwNums = myNums.map((num)=>num*10).map((num)=>num+1).filter((num)=>num>30);
+// console.log(nuwNums);
+
+// let myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// let myTotal = myNums.reduce((acc, currentVal)=> acc+currentVal,0);
+
+// console.log(myTotal);
